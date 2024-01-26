ecbafa677429b7db43c32b52bd2fb58c90ee89be
Uğurcan Çaykara <ugurcancaykara@Ugurcans-MacBook-Pro.local>
2024-01-26 17:06:32 +0300

    add service discovery implementations to movie service since it's going to send requests to 1 or more instance services(only 1 movie service), adjust service register operations at application startup

--- README.md
+++ README.md
@@ -6,3 +6,34 @@
   - Movie metadata service
   - Rating service
   - Movie(Client-Facing API) service  
+
+
+
+## Staring microservices
+
+Run consul locally
+```
+  docker run -d -p 8500:8500 -p 8600:8600/udp --name=dev-consul consul:1.13 agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
+```
+
+Run each microservice by executing
+```
+  go run *.go
+```
+
+
+Optional: You can optionally add some additional instances of each service by running:
+```
+go run *.go --port <PORT>
+```
+* If you run the preceding command, replace `<PORT>` placeholder with unique port numbers that are not in use yet 
+* We used 8081,8082 and 8083, so you can run with port numbers starting with 8084
+
+
+
+## Testing API
+
+To test API requests, ensure you have at least one healthy instance of each service and make the following request to a movie service
+```
+  curl -v localhost:8083/movie?id=1
+```
--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/ugurcancaykara/odd-service/pkg/discovery"
+	"github.com/ugurcancaykara/odd-service/pkg/discovery/consul"
 	"github.com/ugurcancaykara/odd-service/rating/internal/controller/rating"
 	httphandler "github.com/ugurcancaykara/odd-service/rating/internal/handler/http"
 	"github.com/ugurcancaykara/odd-service/rating/internal/repository/memory"
 )
 
+const serviceName = "rating"
+
 func main() {
-	log.Println("Starting the rating service")
+	var port int
+	flag.IntVar(&port, "port", 8082, "API handler port")
+	flag.Parse()
+	log.Printf("Starting the rating service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
 	repo := memory.New()
 	ctrl := rating.New(repo)
 	h := httphandler.New(ctrl)
 	http.Handle("/rating", http.HandlerFunc(h.Handle))
-	if err := http.ListenAndServe(":8082", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
 }
