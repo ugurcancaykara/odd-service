c5baaae296dfcca074ff25d91c59c9f480f84e5d
Uğurcan Çaykara <ugurcancaykara@Ugurcans-MacBook-Pro.local>
2024-01-28 13:12:41 +0300

    adjust service startups to use grpc gateways

--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -5,14 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
+	"net"
 	"time"
 
+	"github.com/ugurcancaykara/odd-service/gen"
 	"github.com/ugurcancaykara/odd-service/pkg/discovery"
 	"github.com/ugurcancaykara/odd-service/pkg/discovery/consul"
 	"github.com/ugurcancaykara/odd-service/rating/internal/controller/rating"
-	httphandler "github.com/ugurcancaykara/odd-service/rating/internal/handler/http"
+	grpchandler "github.com/ugurcancaykara/odd-service/rating/internal/handler/grpc"
 	"github.com/ugurcancaykara/odd-service/rating/internal/repository/memory"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 const serviceName = "rating"
@@ -42,9 +45,15 @@ func main() {
 	defer registry.Deregister(ctx, instanceID, serviceName)
 	repo := memory.New()
 	ctrl := rating.New(repo)
-	h := httphandler.New(ctrl)
-	http.Handle("/rating", http.HandlerFunc(h.Handle))
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+	h := grpchandler.New(ctrl)
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	srv := grpc.NewServer()
+	reflection.Register(srv)
+	gen.RegisterRatingServiceServer(srv, h)
+	if err := srv.Serve(lis); err != nil {
 		panic(err)
 	}
 }
